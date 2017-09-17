9c6a0664caa15901850a2b3295e0dde34ee24ea2
sk\skrueger <krueger.sophie@googlemail.com>
2017-09-17 22:09:02 +0200

    changed constructor parameter for class Game from InputStream to byte[]

--- src/main/java/luxmeter/puzzlefx/model/Game.java
+++ src/main/java/luxmeter/puzzlefx/model/Game.java
@@ -1,6 +1,8 @@
 package luxmeter.puzzlefx.model;
 
 import javafx.scene.image.Image;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.util.List;
@@ -11,27 +13,35 @@ import java.util.List;
  */
 public class Game implements Serializable {
 
+    private static final Logger LOG = LoggerFactory.getLogger(Game.class);
     private byte[] image;
     private Image resizedImage;
     private List<Piece> piecesInNormalOrder;
     private List<Piece> shuffledPieces;
 
-    //TODO: byte[] übergeben statt Stream
-    public Game(InputStream imageFilePath) {
+    public Game(byte[] imageByteArray) {
+        this.image = imageByteArray;
+
+        InputStream inputStream = new ByteArrayInputStream(imageByteArray);
         boolean preserveRatio = true;
         boolean smooth = true;
 
-        this.resizedImage = new Image(imageFilePath,
+        this.resizedImage = new Image(inputStream,
                 AppConstants.MAX_WINDOW_WIDTH,
                 AppConstants.MAX_WINDOW_HEIGHT,
                 preserveRatio,
                 smooth);
 
-        this.piecesInNormalOrder = Piece.createPieces(resizedImage,
-                AppConstants.NUM_HORIZONTAL_SLICES,
-                AppConstants.NUM_VERTICAL_SLICES);
-        this.shuffledPieces = Piece.shufflePieces(piecesInNormalOrder);
-
+        if (resizedImage.getPixelReader() == null) {
+            LOG.error("Something is wrong with the selected image");
+            this.resizedImage = null;
+        }
+        else {
+            this.piecesInNormalOrder = Piece.createPieces(resizedImage,
+                    AppConstants.NUM_HORIZONTAL_SLICES,
+                    AppConstants.NUM_VERTICAL_SLICES);
+            this.shuffledPieces = Piece.shufflePieces(piecesInNormalOrder);
+        }
     }
 
     public Image getResizedImage() {
